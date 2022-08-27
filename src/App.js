8e0db17b445f63206d91122642e224ca46a593dd
Sofía Cobo <sofiamariacobo@gmail.com>
2022-08-27 00:51:17 -0300

    add admin page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-//import React, {useState} from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header/Header'
 import { Route, Routes } from "react-router-dom";
 //import Home from "./pages/home" 
 import Footer from './components/Footer/Footer';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import Admin from './pages/admin';
 
 function App() {
-  //const [isLogin, setIsLogin] = useState();
+  const [isLogin, setIsLogin] = useState();
   return (
     <>
       <Header/>
       <Routes>
         <Route path='/registrarse' element={<Register/>}/>
         <Route path='/' element={<Login/>}/>
+        <Route path='/admin' element={<Admin/>}/>
         
         {/* <Route path='/login' element={<Login/>}/> */}
       </Routes> 
