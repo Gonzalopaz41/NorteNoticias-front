a9b37e76d090eae43cde4356281e4d2dd9bb3465
Gonzalopaz41 <gonzalopazok@gmail.com>
2022-09-07 11:11:30 -0300

    app.js file modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {Route, Routes } from "react-router-dom";
 import Home from './pages/home' 
 import Footer from './components/Footer/Footer';
 import Register from './components/Register/Register';
-//import Login from './components/Login/Login';
 import Login from './pages/login'
+import Admin from './pages/admin'
 
 function App() {
   const [isLogin] = useState(localStorage.getItem('token') || false);
@@ -16,6 +16,7 @@ function App() {
         <Route path='/registrarse' element={<Register/>}/>
         <Route path='/' element={isLogin ? <Home/> : <Login/>}/>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/admin' element={<Admin/>}/>
       </Routes> 
       <Footer/> 
     </>
